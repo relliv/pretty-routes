60cf1a38f8eb3002b3eccbfbc70cdc848a366da6
Andrey Helldar <helldar@ai-rus.com>
2020-11-26 19:20:32 +0300

    Refactoring in progress

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 
     <title>{{ \PrettyRoutes\Facades\Trans::get('title') }} | {{ config('app.name') }}</title>
@@ -8,16 +9,20 @@
     <link rel="dns-prefetch" href="https://fonts.googleapis.com">
     <link rel="dns-prefetch" href="https://cdn.jsdelivr.net">
 
-    @include('pretty-routes::favicon')
-    @include('pretty-routes::styles')
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
 </head>
 <body>
 
-<div id="app">
-    @include('pretty-routes::vue')
-</div>
+<div id="app"></div>
 
-@include('pretty-routes::scripts')
+<script>
+    window.trans = {!! json_encode(\PrettyRoutes\Facades\Trans::all(), JSON_UNESCAPED_UNICODE) !!};
+    window.dark = '{{ config("pretty-routes.color_scheme", "auto") }}';
+    window.routesUrl = '{{ route("pretty-routes.list") }}';
+</script>
+
+<script src="https://cdn.jsdelivr.net/gh/andrey-helldar/pretty-routes-frontend@1.21/dist/app.js"></script>
 
 </body>
 </html>
--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -95,42 +95,6 @@
             hide-details
             clearable
         ></v-text-field>
-
-        <v-spacer></v-spacer>
-        <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    :class="{ rotate: loading }"
-                    :disabled="loading"
-                    icon
-                    @click="getRoutes"
-                >
-                    <v-icon>mdi-refresh</v-icon>
-                </v-btn>
-            </template>
-            <span v-text="trans('refreshRoutes')"></span>
-        </v-tooltip>
-
-        <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="openGitHubRepository"
-                    icon
-                >
-                    <v-avatar size="36">
-                        <img
-                            :src="repository.icon"
-                            alt="Github Project Page"
-                        >
-                    </v-avatar>
-                </v-btn>
-            </template>
-            <span v-text="trans('openGitHub')"></span>
-        </v-tooltip>
     </v-app-bar>
 
     <v-main>
