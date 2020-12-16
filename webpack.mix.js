c82033dab6d199eb5cc13bbd7cdbf9a6b6d5c2a7
Andrey Helldar <helldar@ai-rus.com>
2020-12-16 19:03:00 +0300

    Prepare to split

---
+++ webpack.mix.js
@@ -0,0 +1,14 @@
+const mix = require('laravel-mix');
+const config = require('./webpack.config');
+
+const public_path = './dist/';
+const assets = './resources/';
+
+mix
+    .webpackConfig(config)
+
+    .setPublicPath(public_path)
+
+    .js(assets + 'js/app.js', public_path)
+    .version()
+    .sourceMaps();
